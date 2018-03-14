ea16e9a152d44de682250aa13c02f0e0af475e69
smndiaye <serigne.ndiaye@monstar-lab.com>
2018-03-14 19:26:23 +0900

    refs #grape-setup add message model

--- app/api/chat/entities/v1/user.rb
+++ app/api/chat/entities/v1/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Chat
   module Entities
     module V1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :sent_messages,     class_name: 'Message', foreign_key: 'sender_id'
+  has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
 end
