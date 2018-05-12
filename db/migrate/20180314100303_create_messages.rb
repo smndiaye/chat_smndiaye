1acead4aa13226760ad895125608d3278ec57bec
smndiaye <serigne.ndiaye@monstar-lab.com>
2018-05-13 00:59:44 +0900

    refs: database modification

--- db/migrate/20180314100303_create_messages.rb
+++ db/migrate/20180314100303_create_messages.rb
@@ -3,7 +3,7 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-      t.string :content, limit: 1000
+      t.string  :content, limit: 1000
       t.integer :sender_id, references: 'user'
       t.integer :receiver_id, references: 'user'
 
