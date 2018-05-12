83e24fceccc399cb0c918b9b446ad3d14131b1f9
smndiaye <serigne.ndiaye@monstar-lab.com>
2018-05-13 00:59:44 +0900

    refs: database modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@
 class User < ApplicationRecord
   has_many :sent_messages,     class_name: 'Message', foreign_key: 'sender_id'
   has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
+
+  validates :age,      presence: true,
+                       numericality: { greater_than_or_equal_to: 18 }
+  validates :gender,   presence: true
+  validates :city,     presence: true
+  validates :country,  presence: true
+  validates :username, presence: true, uniqueness: true
+
+  enum gender: { male: 0, female: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,25 +12,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_14_100303) do
-
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content", limit: 1000
-    t.integer "sender_id"
-    t.integer "receiver_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_180_314_100_303) do
+  create_table 'messages', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'content', limit: 1000
+    t.integer 'sender_id'
+    t.integer 'receiver_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "username"
-    t.integer "age"
-    t.string "sex"
-    t.string "city"
-    t.string "country"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'username', null: false
+    t.integer 'age', null: false
+    t.string 'gender', null: false
+    t.string 'city', null: false
+    t.string 'country', null: false
+    t.string 'password_digest', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
