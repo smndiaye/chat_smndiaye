eda0d5808d45a09d2a67e874541895d5c4596bee
smndiaye <serigne.ndiaye@monstar-lab.com>
2018-05-17 22:58:54 +0900

    refs: tags release after deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,6 +8,6 @@ set :bundle_without, 'production'
 server '10.10.30.178', user: 'ec2-user', roles: %w[web app]
 
 set :ssh_options, keys: %w[~/.ssh/id_rsa_11cba49f92d1565316b56b3456d6e43a],
-    forward_agent: true,
-    auth_methods: %w[publickey],
-    proxy: Net::SSH::Proxy::Command.new('ssh -o StrictHostKeyChecking=no jotaay-bastion -W %h:%p')
+                  forward_agent: true,
+                  auth_methods: %w[publickey],
+                  proxy: Net::SSH::Proxy::Command.new('ssh -o StrictHostKeyChecking=no jotaay-bastion -W %h:%p')
